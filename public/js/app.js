60644cdbcf04f3083905576558bdf4055871c695
JoshEdgell <josh.0.edgell@gmail.com>
2018-11-12 13:17:11 -0500

    angular controller can get all jokes

--- public/js/app.js
+++ public/js/app.js
@@ -2,5 +2,17 @@ const app = angular.module('DadJokes', []);
 
 app.controller('MainController', ['$http', function($http){
   const controller = this;
-  this.word = 'poop';
+  this.url = 'http://localhost:3000/';
+  this.getAllJokes = function(){
+    $http({
+      method: 'GET',
+      url: this.url + 'jokes',
+    }).then(
+      function(res){
+        console.log(res.data, 'response from this.getAllJokes');
+      }, function(err){
+        console.log(err, 'error from this.getAllJokes');
+      })
+  };
+  this.getAllJokes();
 }])
