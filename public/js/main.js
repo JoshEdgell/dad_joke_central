f5a092aed59f9ce1c3cee4051ce3b98362287221
JoshEdgell <josh.0.edgell@gmail.com>
2019-02-08 07:08:23 -0500

    dad is back in the app

--- public/js/main.js
+++ public/js/main.js
@@ -7,4 +7,12 @@ $(()=>{
     $hamburger.click();
   });
 
+  let $dadBox = $('#dadBox');
+  $dadBox.css('height', $dadBox[0].scrollWidth);
+
+  $(window).on('resize', function(){
+    let $dadBox = $('#dadBox')
+    $dadBox.css('height', $('#dadBox')[0].scrollWidth);
+  })
+
 })
