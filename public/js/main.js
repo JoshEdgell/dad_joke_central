0a42482bbb02c284df7347ae9d128ca419d138e7
JoshEdgell <josh.0.edgell@gmail.com>
2019-02-10 10:05:56 -0500

    navbar height and dadBox fills top to bottom

--- public/js/main.js
+++ public/js/main.js
@@ -8,12 +8,17 @@ $(()=>{
   });
 
   let $dadBox = $('#dadBox');
-  $dadBox.css('height', $dadBox[0].scrollWidth);
+  let $dadBoxWidth = $dadBox[0].scrollWidth;
+  $dadBox.css('height', $dadBoxWidth);
+  let $navbar = $('.pos-f-t');
+  $navbar.css('max-height', 'calc(100% - ' + $dadBoxWidth + 'px)').css('overflow','hidden');
 
   // I'm going to have to put a lot into this resize function.  For example, the height of the navbar is going to have to have a max-height that is 100% of the screen size minus the height of dadBox
   $(window).on('resize', function(){
-    let $dadBox = $('#dadBox')
+    // let $dadBox = $('#dadBox')
+    let $dadBoxWidth = $dadBox[0].scrollWidth;
     $dadBox.css('height', $('#dadBox')[0].scrollWidth);
+    $navbar.css('max-height', 'calc(100% - ' + $dadBoxWidth + 'px)').css('overflow','hidden');
   })
 
 })
